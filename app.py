4ef12c016d9aa1a08859a0e73cb225a9e0f29cb9
michael <mparkcode@gmail.com>
2019-07-17 19:51:17 +0100

    Added about page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def say_hi():
-    return "Hello World"
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
         app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 8080)), debug=True)
